e6bc6c0bd8c3066cc14df63d4a4af0013131e632
Amir <mrc2121@gmail.com>
2024-02-20 17:01:31 +0500

    1

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    function view_upload()
+    {
+        return view('upload');
+    }
+
+    function upload_photo(Request $req) {
+
+        if ($req->file('photo')) {
+            $req->file('photo')->store('public');
+        }
+
+        return 'success';
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\PatientController;
 use Illuminate\Support\Facades\Route;
@@ -16,12 +17,18 @@ Route::get('/edit_patient/{id}', [PatientController::class, 'view_edit_patient']
 
 Route::post('/update_patient/{id}', [PatientController::class, 'save_edit_patient']);
 
-Route::get('/doctors', [DoctorController::class,'view_doctors']);
+Route::get('/doctors', [DoctorController::class, 'view_doctors']);
 
-Route::post('/save_doctor', [DoctorController::class,'create_doctor']);
+Route::post('/save_doctor', [DoctorController::class, 'create_doctor']);
 
-Route::get('/delete_doctor/{id}', [DoctorController::class,'destroy']);
+Route::get('/delete_doctor/{id}', [DoctorController::class, 'destroy']);
 
-Route::get('/edit_doctor/{id}', [DoctorController::class,'view_edit_doctor']);
+Route::get('/edit_doctor/{id}', [DoctorController::class, 'view_edit_doctor']);
 
-Route::post('/update_doctor/{id}', [DoctorController::class,'update_doctor']);
+Route::post('/update_doctor/{id}', [DoctorController::class, 'update_doctor']);
+
+// _____________________________IMAGE_________________________________
+
+Route::get('/upload', [Controller::class, 'view_upload']);
+
+Route::post('/upload_file', [Controller::class, 'upload_photo']);
